7e52420ebefed5610016c330703c6a9256de299a
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-07-15 19:55:00 +0900

    シート画像を生成するスクリプト

--- sheetImageGenerator.php
+++ sheetImageGenerator.php
@@ -33,5 +33,29 @@ for($i = 0; $i < count($sheet); $i++) {
 
 // リクエストされているサイズを取得
 $size = $_REQUEST['size'];
+// ベースサイズと同じなら何もしない
+if($size == GD_BASE_SIZE) {
+  $out = $destinationImage;
+// 違うサイズの場合
+} else {
+  // リクエストされたサイズの空の画像を生成
+  $out = imagecreatetruecolor($size, $size);
+  // リサイズしながら合成
+  imagecopyresampled($out, $destinationImage, 0, 0, 0, 0, $size, $size, GD_BASE_SIZE, GD_BASE_SIZE);
+}
+
+// 出力のバッファリングを有効に
+ob_start();
+// バッファに出力
+imagepng($out, null, 9);
+// バッファから画像を取得
+$content = ob_get_contents();
+// バッファを消去し出力のバッファリングをオフ
+ob_end_clean();
+
+// 出力のタイプを指定
+header('Content-type: image/png');
+// 画像を出力
+echo $content;
 
  ?>
