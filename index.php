00a7ff60ab107e735a6b7fb3c50f240e03679728
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-07-18 00:07:46 +0900

    ビンゴの進行を処理する

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 // Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 // テーブル名を定義
-define('TABLE_NAME_SHEETS' , 'sheets');
+define('TABLE_NAME_SHEETS', 'sheets');
+define('TABLE_NAME_ROOMS', 'rooms');
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
