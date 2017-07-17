2b1af9bf7afcce7a42cd3c3b791a6a72ef42886b
LAPTOP-8DJ1QH41\achak <kogeusagi12@gmail.com>
2017-07-17 23:36:18 +0900

    line66

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ foreach ($events as $event) {
     else if(substr($event->getText(), 4) == 'enter') {
       // ユーザーが未入室の時
       if(getRoomIdOfUser($event->getUserId()) === PDO::PARAM_NULL) {
-        replyTextMessage($bot, $event->getReplyToken(), 'ルームIDを入力してください。')
+        replyTextMessage($bot, $event->getReplyToken(), 'ルームIDを入力してください。');
       } else {
         replyTextMessage($bot, $event->getReplyToken(), '入室済みです。');
       }
